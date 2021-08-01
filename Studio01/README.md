54b12cfdca2a9f77d5807ad028079e3ea1614c7a
Carolista <cjones.renaissancewoman@gmail.com>
2021-08-01 11:31:16 -0500

    Created studio 4 project and changed content in app.js

--- Studio01/README.md
+++
@@ -1,3 +0,0 @@
-##STUDIO 01 - FRONT END TRACK
-
-Here are solutions for all three JS problems, with multiple approaches, the use of new JS syntax, and addressing edge cases.
---
+++ studio04-state/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Healthy Habits</h1>
+      </header>
+      <main>
+        
+          <table>
+              <tr>
+                  <td></td>
+                  <td></td>
+              </tr>
+              <tr>
+                  <td></td>
+                  <td></td>
+              </tr>
+          </table>
+      </main>
+    </div>
+  );
+}
+
+export default App;
